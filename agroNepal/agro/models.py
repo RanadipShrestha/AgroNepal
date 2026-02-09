249554de78a9bbacddb93122aa40c161749727a0
RanadipShrestha <158831555+RanadipShrestha@users.noreply.github.com>
2026-02-09 20:00:53 +0545

    Updated: Adding the User Knowldge Shareing model that store user post data

--- agroNepal/agro/models.py
+++ agroNepal/agro/models.py
@@ -156,3 +156,30 @@ class CropSale(models.Model):
 
     def __str__(self):
         return f"Sale ₹{self.amount} for {self.user_crop.crop.name} ({self.user_crop.planted_date})"
+
+class ShareKnowledge(models.Model):
+  image = models.ImageField(upload_to="img/UserPost", null=True, blank=True)
+  title = models.CharField(max_length=255)
+  slug = models.SlugField(unique=True, blank=True)
+  description = models.TextField()
+  author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+  create_date = models.DateField(auto_now_add=True)
+  updated_date = models.DateTimeField(auto_now=True)
+  blog_content = models.TextField()
+
+  def __str__(self):
+    return self.title
+
+  def save(self, *args, **kwargs):
+    if not self.slug:
+      self.slug = self.generate_unique_slug()
+    super().save(*args, **kwargs)
+  
+  def generate_unique_slug(self): #generate the unique slug for every blog
+    base_slug = slugify(self.title)
+    slug = base_slug
+    num = 1
+    while Blog.objects.filter(slug=slug).exists():
+      slug = f"{base_slug}-{num}"
+      num+=1
+    return slug
