074eb5fe5c9d54ead9d0ba24fbce67a93aaa8f4d
RanadipShrestha <158831555+RanadipShrestha@users.noreply.github.com>
2026-03-18 19:21:40 +0545

    fix, Add: fixing the redirect the user accoding to role and Add Header and Sidbar in the Admin Pannel

--- agroNepal/adminDashboard/urls.py
+++ agroNepal/adminDashboard/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-   path("", adminDashboard, name="adminDashboard")
+   path("", adminDashboard, name="admindashboard"), 
 ]
--- agroNepal/adminDashboard/views.py
+++ agroNepal/adminDashboard/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def adminDashboard(request):
-  return render(request, 'adminDashboard/dashboard.html')
+  return render(request, 'adminDashboard/dashboard/dashboard.html')
