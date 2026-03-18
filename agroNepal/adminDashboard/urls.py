c715288ba10d1a01e9be90a93fd56a5df32d8e77
RanadipShrestha <158831555+RanadipShrestha@users.noreply.github.com>
2026-03-18 21:09:24 +0545

    Add: Added a page in admin dashboard where all the User Information are displayed

--- agroNepal/adminDashboard/urls.py
+++ agroNepal/adminDashboard/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-   path("", adminDashboard, name="admindashboard"), 
+   path("", adminDashboard, name="admindashboard"),
+    path("users/", adminUsers, name="adminUsers"), 
 ]
--- agroNepal/adminDashboard/views.py
+++ agroNepal/adminDashboard/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from user.models import CustomUser
 
 # Create your views here.
 def adminDashboard(request):
   return render(request, 'adminDashboard/dashboard/dashboard.html')
+
+def adminUsers(request):
+  users = CustomUser.objects.all().order_by('-date_joined')
+  return render(request, 'adminDashboard/dashboard/user/users.html', {'users': users}) 
